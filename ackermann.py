34d12a16e31216dc0a328812e3e221f48bc0981e
Jimmy Mårdell <jimmy.mardell@gmail.com>
2020-12-27 22:44:15 +0100

    Cleanup

--- ackermann.py
+++ ackermann.py
@@ -7,10 +7,6 @@ MOD = 32768
 ack = [[0] * MOD for i in range(5)]
 
 for Z in range(1, MOD):
-    #ack = defaultdict(lambda: defaultdict(int))
-    if Z % 100 == 0:
-        print(" at ", Z)
-
     for a in range(0, 5):
         for b in range(0, 32768):
             if a == 0:
@@ -18,11 +14,8 @@ for Z in range(1, MOD):
             elif b == 0:
                 v = ack[a-1][Z]
             else:
-                w = ack[a][b-1]
-                #print(w)
-                v = ack[a-1][w]
+                v = ack[a-1][ack[a][b-1]]
             ack[a][b] = v
 
     if ack[4][1] == 6:
         print("found! ", Z)
-
